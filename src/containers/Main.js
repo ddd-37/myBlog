4196fd305a464ac3b1ae8fafcfb97c41d18e23fe
Devon Deason <devon.d.deason@gmail.com>
2020-01-09 11:26:26 -0700

    setup axios

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
+
+// COMPONENTS
 import PostList from "../components/PostList/PostList";
 import PostCurrent from "../components/PostCurrent/PostCurrent";
 
-function Main() {
-  return (
-    <div className="Main">
-      <PostList />
-      <PostCurrent />
-    </div>
-  );
+// UTILS
+import axiosRequest from "./../utils/axios/axios";
+
+class Main extends Component {
+  state = {
+    posts: null
+  };
+
+  componentDidMount() {
+    axiosRequest("/posts").then(response => {
+      console.log(response);
+    });
+  }
+
+  render() {
+    return (
+      <div className="Main">
+        <PostList />
+        <PostCurrent />
+      </div>
+    );
+  }
 }
 
 export default Main;
