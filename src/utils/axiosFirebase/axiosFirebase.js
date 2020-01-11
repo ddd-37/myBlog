275621e72699889cb8c843f95799f4a92e54335a
Devon Deason <devon.d.deason@gmail.com>
2020-01-11 09:24:14 -0700

    add initla setup for firebase

---
+++ src/utils/axiosFirebase/axiosFirebase.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const axiosRequest = axios.create({
+  baseURL: "https://myblog-177bd.firebaseio.com"
+});
+
+axios.interceptors.request.use(
+  request => {
+    return request;
+  },
+  error => {
+    console.log("AXIOS request error!", error);
+  }
+);
+
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    console.log("AXIOS response error!", error);
+    return Promise.reject(error);
+  }
+);
+
+export default axiosRequest;
