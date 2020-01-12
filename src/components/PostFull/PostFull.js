726a6d7d395b58620fa9292ad47a53a67ea699a7
Devon Deason <devon.d.deason@gmail.com>
2020-01-12 08:35:05 -0700

    add button

--- src/components/PostFull/PostFull.js
+++ src/components/PostFull/PostFull.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../UI/Button/Button";
 
 const FullPost = props => {
   console.log("TCL: FullPost", props);
@@ -14,6 +15,7 @@ const FullPost = props => {
       <h1>{data.content.title}</h1>
       <h2>Writen by {data.content.author} on Jan, 11, 2020</h2>
       <p>{data.content.body}</p>
+      <Button type="cancel" text="Delete this post" />
     </div>
   );
 };
